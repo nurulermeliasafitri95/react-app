f5df98e6c5c89177a01eaa63c779c23214e42423
Elia <elia@viprewards.com.my>
2025-06-16 12:10:17 +0800

    managing state in onclick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 function ListGroup() {
-  let items = ["kedah", "perlis", "selangor"];
-  items = [];
+  let items = ["kedah", "perlis", "selangor", "putrajaya"];
+
+  //State hook is a function to tap into built in features in React
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
-      { items.length === 0 && <p>No item</p> }
+      {items.length === 0 && <p>No item</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
