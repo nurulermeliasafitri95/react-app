2ef098015fb4ebdced00e3b06619acc4665f4327
Elia <elia@viprewards.com.my>
2025-06-16 14:52:22 +0800

    build a button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+  children: string;
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+
+const Button = ({ children, color = "primary", onClick }: Props) => {
+  return (
+    <div>
+      <button className={"btn btn-" + color} onClick={onClick}>
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
